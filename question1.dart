0373434d0b01663e51217fc9e84cb9fb9179a212
Muntasir Rafin <mun.rafin@gmail.com>
2025-11-13 17:05:17 +0600

    U2

--- question1.dart
+++ question1.dart
@@ -1,33 +1,34 @@
-void main(){
-  String name = 'Muntasir' ;
+
+double calculateBMI(double weight, double height) {
+  var b = weight / (height * height);
+  return b;  
+}
+
+String getGrade(int score) {
+  if (score >= 90) {
+    return 'A';
+  } else if (score >= 80) {
+    return 'B';
+  } else if (score >= 70) {
+    return 'C';
+  } else if (score >= 60) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+}
+
+
+void main() {
+  String name = 'Muntasir';
   int score = 75;
-  int age= 20;
+  int age = 20;
   double height = 1.78;
   const double weight = 80;
   bool status = true;
   
-  double calculateBMI(double weight, double height){
-    var b = weight / height * height ;
-    return b;  
-  }
-  var bmi= calculateBMI(weight, height);
+  var bmi = calculateBMI(weight, height);
+  var grade = getGrade(score);
   
-  String getGrade(score) {
-    if (score >= 90) {
-      return 'A';
-    } else if (score >= 80) {
-      return 'B';
-    } else if (score >= 70) {
-      return 'C';
-    } else if (score >= 60) {
-      return 'D';
-    } else {
-      return 'F';
-    }
-  }
-  
- var grade = getGrade(score);
-  
- print('Name: $name, Age: $age, Height: $height, Is Student: $status, BMI: $bmi \nGrade: $grade ');
-  
+  print('Name: $name, Age: $age, Height: $height, Is Student: $status, BMI: $bmi \nGrade: $grade');
 }
